408d72ee786c74e71402e9f1aef22943c8e949f0
chengxuyuanfei <chengxuyuanfei@gmail.com>
2015-09-13 10:24:50 +0800

    迁移proxyip_spider分支中更可靠的proxyiprank到master分支

--- proxyiprank.py
+++ proxyiprank.py
@@ -8,6 +8,7 @@ import logging
 import threading
 import traceback
 import socket
+import binascii
 import os
 from operator import itemgetter
 from multiprocessing.dummy import Pool as ThreadPool
@@ -17,9 +18,9 @@ class ProxyIPRank(object):
 	"""docstring for ProxyIPRank"""
 	proxyip_list = []
 	proxyip_rank_dict = {}
-	proxyip_check_times = 1
+	proxyip_check_times = 10
 	proxyip_check_times_max = 20
-	proxyip_availability_percent = 0.5
+	proxyip_availability_percent = 0.7
 	check_timeout = 10
 	check_target_url = 'http://www.chengxuyuanfei.com/'
 	proxyip_log_path = './proxyiprank.log'
@@ -43,7 +44,7 @@ class ProxyIPRank(object):
 		('User-agent','Mozilla/5.0 (Windows NT 6.2; Win64; x64; rv:27.0) Gecko/20121011 Firefox/27.0')
 	]
 	def __init__(self, proxyip_list_arg, port_arg = 3000):
-		logging.basicConfig(filename=self.proxyip_log_path, level=logging.INFO, format='%(asctime)s %(levelname)s Func:%(funcName)s Line:%(lineno)s \n\t\t%(message)s', datefmt='%Y.%m.%d  %H:%M:%S')
+		logging.basicConfig(filename=self.proxyip_log_path, level=logging.INFO, format='%(asctime)s %(levelname)s \n\t\t%(message)s', datefmt='%Y.%m.%d  %H:%M:%S')
 		logging.info('Start proxyip rank, init ProxyIPRank object')
 		for proxyip_ip, proxyip_port in proxyip_list_arg.items():
 			proxyip_str = str(proxyip_ip) + ':' + str(proxyip_port)
@@ -69,14 +70,15 @@ class ProxyIPRank(object):
 	def set_proxyip_log_path(self, log_path):
 		self.proxyip_log_path = log_path
 
-	def add_proxyip_list(self, proxyip_list_arg):
-		logging.info('Add proxyips list to check')
+	def add_proxyip_list(self, proxyip_dict_arg):
 		self.proxyip_list = []
 		self.proxyip_rank_dict = {}
-		for proxyip_ip, proxyip_port in proxyip_list_arg.items():
+		for proxyip_ip, proxyip_port in proxyip_dict_arg.items():
 			proxyip_str = str(proxyip_ip) + ':' + str(proxyip_port)
 			self.proxyip_list.append(proxyip_str)
 		[self.proxyip_rank_dict.setdefault(proxyip, {'avg_time':0.0, 'availability_rate':0.0, 'disperse_rate':0.0, 'check_record':[]}) for proxyip in self.proxyip_list]
+		logging.info('Add proxyips list to check.\n\t Proxyip_list:%s' % str([proxyip for proxyip in self.proxyip_list]))
+
 
 	def check_proxyip(self, proxyip):
 		check_time = 0
@@ -92,13 +94,13 @@ class ProxyIPRank(object):
 			content = urllib2.urlopen(req, timeout = self.check_timeout).read()
 			end_time = time.time()
 			check_time =  end_time - start_time
-			print check_time
+			print 'checking proxy ip:', proxyip, 'using time:', check_time
 			self.proxyip_rank_dict[proxyip]['check_record'].append(check_time)
 			self.proxyip_rank_dict[proxyip]['lastest_check_time'] = time.strftime('%Y.%m.%d-%H:%M:%S', time.localtime(time.time()))
 			#logging.info(proxyip + '\tsucceeded')
 			time.sleep(3)
 		except Exception, e:
-			print e
+			print 'checking proxy ip:', proxyip, 'Exception: ', e
 			self.proxyip_rank_dict[proxyip]['check_record'].append(0)
 			self.proxyip_rank_dict[proxyip]['lastest_check_time'] = time.strftime('%Y.%m.%d-%H:%M:%S', time.localtime(time.time()))
 			#logging.info(proxyip + '\tfailed')
@@ -110,10 +112,8 @@ class ProxyIPRank(object):
 		print self.proxyip_rank_dict
 		for proxyip, proxyip_check_info in self.proxyip_rank_dict.items():
 			for list_index, check_time in enumerate(proxyip_check_info['check_record']):
-				print 'check_time:', check_time, 'list_index:', list_index
 				if check_time == 0:
 					self.proxyip_rank_dict[proxyip]['check_record'][list_index] = self.check_timeout
-		print self.proxyip_rank_dict
 
 	def rank_proxyips(self):
 		for proxyip, proxyip_check_info in self.proxyip_rank_dict.items():
@@ -124,11 +124,12 @@ class ProxyIPRank(object):
 			self.proxyip_rank_dict[proxyip]['disperse_rate'] = math.sqrt(self.proxyip_rank_dict[proxyip]['disperse_rate'] / len(proxyip_check_info['check_record']))
 
 	def save_to_disk(self, record_save_path = './proxyiprank.record.json', available_ip_sava_path = './proxyiprank.availability.json'):
+		print [ip for ip, value in self.proxyip_rank_dict.items()],[value['check_record'] for ip, value in self.proxyip_rank_dict.items()]
 		self.record_save_path = record_save_path
 		self.available_ip_sava_path = available_ip_sava_path
 		if os.path.isfile(record_save_path) == False:
 			with open(record_save_path, 'a+') as fd:
-				fd.write(json.dumps(self.proxyip_rank_dict))
+				fd.write(json.dumps(self.proxyip_rank_dict, indent = 4))
 				logging.info('Save proxyiprank record to ' + record_save_path)
 		else:
 			with open(record_save_path, 'a+') as fd:
@@ -147,115 +148,116 @@ class ProxyIPRank(object):
 				json.dump(available_ips, fd, indent = 4)
 				logging.info('Save available proxyips to ' + available_ip_sava_path)
 		else:
+			print [ip for ip, value in self.proxyip_rank_dict.items()],[value['check_record'] for ip, value in self.proxyip_rank_dict.items()]
 			with open(available_ip_sava_path, 'r') as fd:
 				available_ips_file = json.load(fd)
 				available_ips = {}
 				for proxyip_key, proxyip_value in self.proxyip_rank_dict.items() :
 					if proxyip_value['availability_rate'] >= self.proxyip_availability_percent:
 						available_ips_file[proxyip_key] = proxyip_value
+				print [ip for ip, value in available_ips_file.items()],[value['check_record'] for ip, value in available_ips_file.items()]
 				with open(available_ip_sava_path, 'w') as fd_tmp:
 					json.dump(available_ips_file, fd_tmp, indent = 4)
 				logging.info('Save available proxyips to ' + available_ip_sava_path)
-		# with open(record_save_path, 'a+') as fd:
-		# 	proxyip_rank_dict_file = json.load(fd)
-		# 	for proxyip_key, proxyip_value in self.proxyip_rank_dict.items():
-		# 		proxyip_rank_dict_file[proxyip_key] = proxyip_value
-		# 	with open(record_save_path, 'w') as fd_tmp:
-		# 		fd_tmp.write(json.dumps(proxyip_rank_dict_file))
-		# 	logging.info('Save proxyiprank record to ' + record_save_path)
-		# with open(available_ip_sava_path, 'r') as fd:
-		# 	available_ips_file = json.load(fd)
-		# 	available_ips = {}
-		# 	for proxyip_key, proxyip_value in self.proxyip_rank_dict.items() :
-		# 		if proxyip_value['availability_rate'] >= self.proxyip_availability_percent:
-		# 			available_ips_file[proxyip_key] = proxyip_value
-		# 	with open(available_ip_sava_path, 'w') as fd_tmp:
-		# 		json.dump(available_ips_file, fd_tmp, indent = 4)
-		# 	logging.info('Save available proxyips to ' + available_ip_sava_path)
-		
 
 	def start_check_proxyips(self):
 		checked_times = 0
 		while checked_times < self.proxyip_check_times:
-			# check_pool = ThreadPool()
-			# check_pool.map(self.check_proxyip, self.proxyip_list)
-			# checked_times = checked_times + 1
-			# check_pool.close()
-			# check_pool.join()
-			threads = []
-			for index in range(len(self.proxyip_list)):
-				tmp = self.proxyip_list[index]
-				check_thread = threading.Thread(target = self.check_proxyip, args = (tmp,))
-				threads.append(check_thread)
-			for index in range(len(self.proxyip_list)):
-				threads[index].start()
-			for index in range(len(self.proxyip_list)):
-				threads[index].join()
+			check_pool = ThreadPool(len(self.proxyip_list))
+			check_pool.map(self.check_proxyip, self.proxyip_list)
 			checked_times = checked_times + 1
+			check_pool.close()
+			check_pool.join()
+			# threads = []
+			# for index in range(len(self.proxyip_list)):
+			# 	tmp = self.proxyip_list[index]
+			# 	check_thread = threading.Thread(target = self.check_proxyip, args = (tmp,))
+			# 	threads.append(check_thread)
+			# for index in range(len(self.proxyip_list)):
+			# 	threads[index].start()
+			# for index in range(len(self.proxyip_list)):
+			# 	threads[index].join()
+			# checked_times = checked_times + 1
 		self.flush_proxyips_dict()
 		self.rank_proxyips()
-		#[availability for list_index, availability in enumerate(proxyip_check_info['check_record']) if proxyip_check_info['check_record'][list_index] < self.check_timeout]
 		logging.info('Checked ' + str(len(self.proxyip_rank_dict)) + ' ips. ' + str(len([available_ip for available_ip, proxyip_check_info in self.proxyip_rank_dict.items() if proxyip_check_info['availability_rate'] >= self.proxyip_availability_percent]))+ ' is available.')
-		print self.proxyip_rank_dict
+		print 'proxyip_rank: ', self.proxyip_rank_dict
 
 	def start_proxyip_server(self, port_arg = 3000):
 		server_ip = ''
 		server_port = port_arg
-		server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 		server_socket.bind((server_ip, server_port))
+		server_socket.listen(5)
 		while True:
 			try:
-				client_message, client_address = server_socket.recvfrom(1024)
-				print client_address
+				client_socket, client_address = server_socket.accept()
+				print 'Tcp accept client: ', client_address
 				logging.info('Accept client: ' + client_address[0])
-				send_json_thread = threading.Thread(target = self.send_json_to_client, args = (server_socket, client_address))
+				send_json_thread = threading.Thread(target = self.send_json_to_client, args = (client_socket,))
+				send_json_thread.setDaemon(True)
 				send_json_thread.start()
-				# with open(self.available_ip_sava_path, 'r') as fd:
-				# 	available_ips = json.load(fd)
-				# # for proxyip_key, proxyip_value in self.proxyip_rank_dict.items() :
-				# # 	if proxyip_value['availability_rate'] >= self.proxyip_availability_percent:
-				# # 		available_ips.setdefault(proxyip_key, proxyip_value)
-				# available_ips_str = json.dumps(available_ips, indent = 4)
-				# server_socket.sendto(available_ips_str, client_address)
-				# logging.info('Send successful')
 			except Exception, e:
 				print 'Exception: ', e
 				logging.exception(e)
-			#time.sleep(1)
 
-	def send_json_to_client(self, server_socket, client_address):
+	def send_json_to_client(self, client_socket):
 		try:
-			print 'send_json_to_client', server_socket, client_address
+			print 'send_json_to_client', client_socket
 			with open(self.available_ip_sava_path, 'r') as fd:
 				available_ips = json.load(fd)
-				# for proxyip_key, proxyip_value in self.proxyip_rank_dict.items() :
-				# 	if proxyip_value['availability_rate'] >= self.proxyip_availability_percent:
-				# 		available_ips.setdefault(proxyip_key, proxyip_value)
 				available_ips_str = json.dumps(available_ips, indent = 4)
-				server_socket.sendto(available_ips_str, client_address)
+				available_ips_check_sum = '{:8x}'.format(binascii.crc32(available_ips_str))
+				client_socket.sendall(available_ips_check_sum)
+				socket_buf_size = 1024
+				while available_ips_str:
+					#print len(available_ips_str)
+					client_socket.sendall(available_ips_str[:socket_buf_size])
+					available_ips_str = available_ips_str[socket_buf_size:]
+				print 'Send successful'
 				logging.info('Send successful')
 		except Exception, e:
 			print 'Exception: ', e
 			logging.exception(e)
+		finally:
+			client_socket.close()
 
 
 	
 
-start_time = time.time()
-proxyip_dict = {}
-fd = open('/root/proxy_ips', 'r')
-for line_index in range(3000):
-	ip = fd.readline()
-	port = fd.readline()
-	proxyip_dict.setdefault(ip.strip(), port.strip())
-fd.close()
-checking_test = ProxyIPRank(proxyip_dict, 5000)
-checking_test.start_check_proxyips()
-print "Using time:", time.time() - start_time
-checking_test.save_to_disk()
-checking_test.add_proxyip_list(proxyip_dict)
-checking_test.start_check_proxyips()
+# start_time = time.time()
+# proxyip_dict = {}
+# fd = open('/root/proxy_ips', 'r')
+# for line_index in range(5):
+# 	ip = fd.readline()
+# 	port = fd.readline()
+# 	proxyip_dict.setdefault(ip.strip(), port.strip())
+# fd.close()
+# checking_test = ProxyIPRank(proxyip_dict, 5000)
+# checking_test.start_check_proxyips()
+# print "Using time:", time.time() - start_time
+# checking_test.save_to_disk()
+# checking_test.add_proxyip_list(proxyip_dict)
+# checking_test.start_check_proxyips()
+# checking_test.save_to_disk()
+# print "Using time:", time.time() - start_time
+# checking_test.add_proxyip_list(proxyip_dict)
+# checking_test.start_check_proxyips()
+# checking_test.save_to_disk()
+# print "Using time:", time.time() - start_time
+# checking_test.add_proxyip_list(proxyip_dict)
+# checking_test.start_check_proxyips()
+# checking_test.save_to_disk()
+# print "Using time:", time.time() - start_time
+# checking_test.add_proxyip_list(proxyip_dict)
+# checking_test.start_check_proxyips()
+# checking_test.save_to_disk()
+# print "Using time:", time.time() - start_time
+# checking_test.add_proxyip_list(proxyip_dict)
+# checking_test.start_check_proxyips()
+# checking_test.save_to_disk()
+# print "Using time:", time.time() - start_time
 
 		
 
